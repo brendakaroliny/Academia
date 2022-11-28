df457c75e65752ff306acdbaabe199f9eabd776f
brendakaroliny <brendakaroliny6@gmail.com>
2022-11-28 17:28:33 -0300

    Separando o cabeçalho

--- react/academia/src/App.js
+++ react/academia/src/App.js
@@ -1,13 +1,15 @@
 import React,{ Component } from 'react';
 
 import Site from './Site';
+import formulario from './Componentes/formulario';
+import Cabecalho from './Componentes/Cabecalho';
 
 
 function App() {
   return (
     <div className="App">
-      <Site/>
-      
+      <Cabecalho/>
+      <Site/>  
     </div>
   );
 }
--- react/academia/src/Site.js
+++ react/academia/src/Site.js
@@ -6,54 +6,7 @@ const Site = props=>{
     return(
         <div>
 
-       <header className = 'menu1-principal'>
-           <main>
-
-            <div className = 'header1'>
-                <div className ='logo'>
-                   <img src='./img/logo2-removebg-preview.png'/>
-                </div>
-                <div className = 'rede-sociais'>
-                    <ul>
-                    
-                       <li><a href='https://www.linkedin.com/in/brenda-karoliny-52b0b9179/'><img src='./img/instagram.png'/></a></li>
-                       <li><a href='https://www.linkedin.com/in/brenda-karoliny-52b0b9179/'><img src='./img/facebook.png'/></a></li>
-                       <li><a href='https://www.linkedin.com/in/brenda-karoliny-52b0b9179/'><img src='./img/twitter.png'/></a></li>
-                       <li><a href='https://www.linkedin.com/in/brenda-karoliny-52b0b9179/'><img src='./img/youtube.png'/></a></li>
-                       <li><a href='https://www.linkedin.com/in/brenda-karoliny-52b0b9179/'><img src='./img/wpp.png'/></a></li>
-                    </ul>
-                 </div>
-             </div>
-        </main>
-    </header>
-
-    <main className='col-100 menu1-urls'>
-        <div className = 'header2'>
-            <div className = 'menu1'>
-                <ul>
-                    <li>
-                        <a href='./index.html'>Home</a>
-                    </li>
-                   
-                    <li>
-                        <a href='./modalidades.html'>Modalidades</a>
-                    </li>
-                    <li>
-                        <a href='./formulario.html'>Franquia</a>
-                    </li>
-                    <li>
-                        <a href='./planos.html'>Planos</a>
-                    </li>
-                        <li>
-                            <a href='./unidades.html'>Unidades</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className = 'busca'>
-                   <input placeholder='Buscar Aqui...' type='text'/>
-                </div>
-            </div>
-        </main>
+    
         <div className='col-100'>
         <div className='slider-principal'>
             <img src='./img/banner-01.png' />
